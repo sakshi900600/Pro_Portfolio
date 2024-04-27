09dd432365bfa3b2e114b42947f775599293ad27
sakshi900600 <kumarisakshi900600@gmail.com>
2024-04-27 10:28:42 +0530

    first commit

---
+++ js/portfolio.js
@@ -0,0 +1,25 @@
+// js for light and dark theme
+
+var icon = document.getElementById("icon")
+
+icon.onclick = function () {
+    document.body.classList.toggle("dark-mode")
+
+    if (document.body.classList.contains("dark-mode")) {
+        icon.src = "/assets/sun.png"
+    }
+    else {
+        icon.src = "/assets/moon.svg"
+    }
+}
+
+
+//  js for navbar toggle
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function () {
+    navbar = document.querySelector('.nav-bar');
+    navbar.classList.toggle("active")
+}
+
+
+document.addEventListener('contextmenu', event => event.preventDefault());
